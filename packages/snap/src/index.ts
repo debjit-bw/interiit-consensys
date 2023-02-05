9070cbc67d0e2ec2e9b4b20dcca53b370bc13f1b
kartik <mekartik27@gmail.com>
2023-02-06 01:14:47 +0530

    changes

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,4 +1,4 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
+import { OnRpcRequestHandler, OnCronjobHandler } from '@metamask/snap-types';
 
 async function getRate(value, value1) {
   const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=${value}&order=market_cap_desc&per_page=100&page=1&sparkline=false`); 
